12b1992584aa92ded5f54440e43dd75213302fe6
adriel <adrielcarlos1010@gmail.com>
2018-09-10 00:35:55 -0300

    aterações erro

---
+++ RealSolidsMobile2/animacoes/Cone.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import Video from 'react-native-video';
+import cubo from '../videos/cubo.mp4';
+
+export default class Cubo extends Component {
+    render() {
+        return (
+            <Video 
+                source={cubo}
+                style={style.backgroundVideo}
+            />
+        );
+    }
+}
+
+const style = StyleSheet.create({
+    backgroundVideo: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
+});
